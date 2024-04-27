8debac12bbf6529070d4a31e7e5858373abdfd28
MCHCortana <wolokika@gmail.com>
2024-04-27 11:44:17 +0200

    added rings

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const alarmElement = document.querySelector('.alarm');
 const alarmRing = () => {
   alarmElement.classList.add('alarm--ring');
   document.querySelector('audio').play();
+  document.querySelector('audio').play();
+  document.querySelector('audio').play();
 };
 
 alarmElement.addEventListener('click', alarmRing);
