82108b3e60416d6045f094d2633237b401e5f7e5
MCHCortana <wolokika@gmail.com>
2024-04-27 11:51:49 +0200

    minor

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const alarmElement = document.querySelector('.alarm');
 const alarmRing = () => {
   alarmElement.classList.add('alarm--ring');
   document.querySelector('audio').play();
-  document.querySelector('audio').play();
 };
 
 alarmElement.addEventListener('click', alarmRing);
