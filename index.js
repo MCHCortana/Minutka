0577844bb6e3bffce8e14e3853eab000fce44a26
MCHCortana <wolokika@gmail.com>
2024-04-27 11:51:03 +0200

    ring changed

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const alarmRing = () => {
   alarmElement.classList.add('alarm--ring');
   document.querySelector('audio').play();
   document.querySelector('audio').play();
-  document.querySelector('audio').play();
 };
 
 alarmElement.addEventListener('click', alarmRing);
